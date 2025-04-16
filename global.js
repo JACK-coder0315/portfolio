2f7a72190c2ad39f7f1834dcbfbd024de2841730
JACK-coder0315 <206167918+JACK-coder0315@users.noreply.github.com>
2025-04-15 20:30:44 -0700

    2

--- global.js
+++ global.js
@@ -1,47 +1,47 @@
 console.log("IT’S ALIVE!");
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
 }
 
 let navLinks = $$("nav a");
 let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
+    (a) => a.host === location.host && a.pathname === location.pathname
 );
 currentLink?.classList.add("current");
 
 const BASE_PATH =
-  location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "/"
-    : "/portfolio/";
+    location.hostname === "localhost" || location.hostname === "127.0.0.1"
+        ? "/"
+        : "/portfolio/";
 
 let pages = [
-  { url: "", title: "Home" },
-  { url: "about/", title: "About" },
-  { url: "projects/", title: "Projects" },
-  { url: "contact/", title: "Contact" },
-  { url: "https://github.com/JACK-coder0315", title: "GitHub" },
+    { url: "", title: "Home" },
+    { url: "about/", title: "About" },
+    { url: "projects/", title: "Projects" },
+    { url: "contact/", title: "Contact" },
+    { url: "https://github.com/JACK-coder0315", title: "GitHub" },
 ];
 
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
 for (let p of pages) {
-  let url = !p.url.startsWith("http") ? BASE_PATH + p.url : p.url;
-  let a = document.createElement("a");
-  a.href = url;
-  a.textContent = p.title;
+    let url = !p.url.startsWith("http") ? BASE_PATH + p.url : p.url;
+    let a = document.createElement("a");
+    a.href = url;
+    a.textContent = p.title;
 
-  a.classList.toggle(
-    "current",
-    a.host === location.host && a.pathname === location.pathname
-  );
+    a.classList.toggle(
+        "current",
+        a.host === location.host && a.pathname === location.pathname
+    );
 
-  if (a.host !== location.host) {
-    a.target = "_blank";
-  }
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
 
-  nav.append(a);
+    nav.append(a);
 }
 
 document.body.insertAdjacentHTML(
@@ -56,25 +56,41 @@ document.body.insertAdjacentHTML(
       </select>
     </label>
   `
-  );
-  
-  let select = document.querySelector("select");
-  function setColorScheme(scheme) {
+);
+
+let select = document.querySelector("select");
+function setColorScheme(scheme) {
     if (scheme === "normal") {
-      document.documentElement.style.removeProperty("color-scheme");
+        document.documentElement.style.removeProperty("color-scheme");
     } else {
-      document.documentElement.style.setProperty("color-scheme", scheme);
+        document.documentElement.style.setProperty("color-scheme", scheme);
     }
     select.value = scheme;
-  }
+}
 
-  if ("colorScheme" in localStorage) {
+if ("colorScheme" in localStorage) {
     setColorScheme(localStorage.colorScheme);
-  } else {
+} else {
     setColorScheme("normal");
-  }
+}
 
-  select.addEventListener("input", (event) => {
+select.addEventListener("input", (event) => {
     localStorage.colorScheme = event.target.value;
     setColorScheme(event.target.value);
-  });
+});
+
+let form = document.querySelector("form");
+
+form?.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let data = new FormData(form);
+    let params = [];
+
+    for (let [key, value] of data) {
+        params.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+    }
+
+    let mailto = form.action + "?" + params.join("&");
+    location.href = mailto;
+});
