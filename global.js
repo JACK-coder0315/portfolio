895a26bf41e831f90e33ed67dbdada8b3163c022
JACK-coder0315 <206167918+JACK-coder0315@users.noreply.github.com>
2025-04-15 21:46:22 -0700

    Update global.js

--- global.js
+++ global.js
@@ -3,20 +3,32 @@ console.log("IT’S ALIVE!");
 const BASE_PATH = location.hostname.includes("localhost") ? "/" : "/portfolio/";
 
 let pages = [
-    { url: "", title: "Home" },
-    { url: "projects/", title: "Projects" },
-    { url: "contact/", title: "Contact" },
-    { url: "resume/", title: "Resume" },
-    { url: "hiking/hiking.html", title: "Hiking" },
-    { url: "https://github.com/JACK-coder0315", title: "GitHub" }
-  ];
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "hiking/hiking.html", title: "Hiking" },
+  { url: "https://github.com/JACK-coder0315", title: "GitHub" }
+];
 
 let navHTML = `<nav class="nav">\n`;
+
 for (let p of pages) {
   let url = p.url.startsWith("http") ? p.url : BASE_PATH + p.url;
   navHTML += `<a href="${url}"${url.startsWith("http") ? ' target="_blank"' : ''}>${p.title}</a>\n`;
 }
-navHTML += `</nav>`;
+
+navHTML += `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="normal">Auto</option>
+    </select>
+  </label>
+</nav>
+`;
 
 document.getElementById("nav-container")?.insertAdjacentHTML("beforeend", navHTML);
 
@@ -26,20 +38,8 @@ document.querySelectorAll(".nav a").forEach((a) => {
   }
 });
 
-let themeHTML = `
-  <label class="color-scheme">
-    Theme:
-    <select>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-      <option value="normal">Auto</option>
-    </select>
-  </label>
-`;
-
-document.getElementById("nav-container")?.insertAdjacentHTML("beforeend", navHTML + themeHTML);
-
 let select = document.querySelector("select");
+
 function setColorScheme(scheme) {
   if (scheme === "normal") {
     document.documentElement.style.removeProperty("color-scheme");
@@ -48,11 +48,13 @@ function setColorScheme(scheme) {
   }
   select.value = scheme;
 }
+
 if ("colorScheme" in localStorage) {
   setColorScheme(localStorage.colorScheme);
 } else {
   setColorScheme("normal");
 }
+
 select.addEventListener("input", (e) => {
   localStorage.colorScheme = e.target.value;
   setColorScheme(e.target.value);
