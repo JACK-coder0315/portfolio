bc7dbc543e78af7e91bbbc3e85626ad41027da49
JACK-coder0315 <206167918+JACK-coder0315@users.noreply.github.com>
2025-04-27 14:32:47 -0700

    2

--- visualization/visualization.js
+++ visualization/visualization.js
@@ -1,15 +1,33 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-let arcGenerator = d3.arc()
-  .innerRadius(0)    
-  .outerRadius(50);  
+let data = [
+  { value: 1, label: 'Apples' },
+  { value: 2, label: 'Oranges' },
+  { value: 3, label: 'Mangoes' },
+  { value: 4, label: 'Pears' },
+  { value: 5, label: 'Limes' },
+  { value: 5, label: 'Cherries' }
+];
 
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
+
+let sliceGenerator = d3.pie().value(d => d.value);
+let arcData = sliceGenerator(data);
+
+arcData.forEach((d, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arcGenerator(d))
+    .attr('fill', colors(idx));
 });
 
-d3.select('#projects-pie-plot')
-  .append('path')
-  .attr('d', arc)        
-  .attr('fill', 'red');  
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend.append('li')
+    .attr('style', `--color:${colors(idx)}`)
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+});
