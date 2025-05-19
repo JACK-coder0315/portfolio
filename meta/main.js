d2d518575a06e621ed9cdae007e129c1831b7413
JACK-coder0315 <206167918+JACK-coder0315@users.noreply.github.com>
2025-05-19 02:16:47 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -289,7 +289,7 @@ function renderDailyItems(uniqueDays, allCommits) {
   renderDailyItems(uniqueDays, commits);
 
   // 4) 同步滚动：当 scroll-container1 滚动
-  d3.select('#scroll-container1')
+ d3.select('#scroll-container')
     .on('scroll', function() {
       const idx = Math.floor(this.scrollTop / ITEM_HEIGHT);
       const slice = commits.slice(idx, idx + VISIBLE_COUNT);
